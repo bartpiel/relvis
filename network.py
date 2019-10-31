66e1d24692f63220ffce558cdbf876171cf6a55c
JanSawicki <jansawickisawicki@gmail.com>
2019-10-31 21:42:22 +0100

    Added articles 20-30

--- network.py
+++ network.py
@@ -18,16 +18,20 @@ class ELEMENT:
     CLUB = "klub poselski"
     CIRCLE = "koło poselski"
     CHIEF_OF_REGISTARS_OFFICE = "szef kancelarii sejmu"
-    VICECHIEF_OF_REGISTARS_OFFICE = "zastępca szefa kancelarii sejmu"
+    VICE_CHIEF_OF_REGISTARS_OFFICE = "zastępca szefa kancelarii sejmu"
     SENIOR_CONVENT = "konwent seniorów"
     CHIEF_OF_CLUB = "przewodniczący klubu"
     VICE_CHIEF_OF_CLUB = "wiceprzewodniczący klubu"
     SPECIAL_COMMITTEE = "komisja nadzwyczajna"
     FORENSIC_COMMITTEE = "komisja śledcza"
+    CHIEF_OF_COMMITTEE = "szef komisji"
+    PRESIDIUM_OF_COMMITTEE = "prezydium komisji"
+    CHIEF_OF_COMMITTEE = "przedowniczący komisji"
+    VICE_CHIEF_OF_COMMITTEE = "wiceprzedowniczący komisji"
     
 # Relations definition
 class RELATION:
-    INTITUTES = "powołuje"
+    INSTITUTES = "powołuje"
     CHOOSES = "wybiera"
     CONDUCTS_CHOICE = "przeprowadza wybór"
     COMMANDS_CHOICE = "zarządza wybór"
@@ -39,16 +43,21 @@ class RELATION:
     REPRESENTS = "reprezentuje"
     LEADS = "przewodzi"
     HELPS = "udziela pomocy"
+    CANCELS = "odwołuje"
     CONSULTS_WITH = "konsultuje się z"
     SUBSTITUTES = "zastępuje"
     DECIDES_BUDGET = "decyduje o budżecie"
+    WARN = "zwrócić uwagę/upomnieć/udzielić nagany"
+    DECREASE_DIET = "zmniejszyć dietę"
+    EXCLUDE_FROM_PRECEEDINGS = "wykluczyć z obrad"
+
 
 # Graph initialization
 G=nx.DiGraph()
 
 # Relation definitions
 # Art. 1 p. 1
-G.add_edge(ELEMENT.PRESIDENT, ELEMENT.MARSHALL_SENIOR, relation=RELATION.INTITUTES)
+G.add_edge(ELEMENT.PRESIDENT, ELEMENT.MARSHALL_SENIOR, relation=RELATION.INSTITUTES)
 # Art. 2 p. 2
 G.add_edge(ELEMENT.MARSHALL_SENIOR, ELEMENT.TEMPORARY_SECRETARY, relation=RELATION.CHOOSES)
 # Art. 4 p. 3
@@ -62,17 +71,17 @@ G.add_edge(ELEMENT.PRESIDIUM, ELEMENT.SECRETARY, relation=RELATION.SUGGESTS_CAND
 # Art. 6a
 G.add_edge(ELEMENT.PRIME_MINISTER, ELEMENT.COUNCIL_OF_MINISTERS, relation=RELATION.DEMITS)
 # Art. 7 p. 1
-G.add_edge(ELEMENT.REPRESENTATIVE, ELEMENT.COMMITTEE, relation=RELATION.IS_MEMBER, may = TRUE)
+G.add_edge(ELEMENT.REPRESENTATIVE, ELEMENT.COMMITTEE, relation=RELATION.IS_MEMBER, may = True)
 # Art. 7 p. 3
-G.add_edge(ELEMENT.REPRESENTATIVE, ELEMENT.COUNCIL_OF_MINISTERS, relation=RELATION.IS_MEMBER, may = TRUE)
+G.add_edge(ELEMENT.REPRESENTATIVE, ELEMENT.COUNCIL_OF_MINISTERS, relation=RELATION.IS_MEMBER, may = True)
 # Art. 7 p. 2 pdp. 2
 G.add_edge(ELEMENT.REPRESENTATIVE, ELEMENT.COMMITTEE, relation=RELATION.OBEYS_COMMANDS)
 # Art. 7 p. 8 pdp. 4
-G.add_edge(ELEMENT.MARSHALL, ELEMENT.REPRESENTATIVE, relation=RELATION.GRANTS_VACATION, may=TRUE)
+G.add_edge(ELEMENT.MARSHALL, ELEMENT.REPRESENTATIVE, relation=RELATION.GRANTS_VACATION, may=True)
 # Art. 8 p. 1
-G.add_edge(ELEMENT.REPRESENTATIVE, ELEMENT.CLUB, relation=RELATION.IS_MEMBER, may = TRUE)
+G.add_edge(ELEMENT.REPRESENTATIVE, ELEMENT.CLUB, relation=RELATION.IS_MEMBER, may = True)
 # Art. 8 p. 1
-G.add_edge(ELEMENT.REPRESENTATIVE, ELEMENT.CIRCLE, relation=RELATION.IS_MEMBER, may = TRUE)
+G.add_edge(ELEMENT.REPRESENTATIVE, ELEMENT.CIRCLE, relation=RELATION.IS_MEMBER, may = True)
 # Art. 10 p. 1 pdp. 2
 G.add_edge(ELEMENT.MARSHALL, ELEMENT.PARLIMENT, relation=RELATION.REPRESENTS)
 # Art. 10 p. 1 pdp. 2
@@ -109,14 +118,31 @@ G.add_edge(ELEMENT.VICE_CHIEF_OF_CLUB, ELEMENT.SENIOR_CONVENT, relation=RELATION
 # Art. 18 p. 3
 G.add_edge(ELEMENT.MARSHALL, ELEMENT.COMMITTEE, relation=RELATION.DECIDES_BUDGET)
 # Art. 19 p. 1
-G.add_edge(ELEMENT.PARLIMENT, ELEMENT.SPECIAL_COMMITTEE, relation=RELATION.INTITUTES)
+G.add_edge(ELEMENT.PARLIMENT, ELEMENT.SPECIAL_COMMITTEE, relation=RELATION.INSTITUTES)
 G.add_edge(ELEMENT.PARLIMENT, ELEMENT.SPECIAL_COMMITTEE, relation=RELATION.CANCELS)
 # Art. 19a
-G.add_edge(ELEMENT.PARLIMENT, ELEMENT.FORENSIC_COMMITTEE, relation=RELATION.INTITUTES)
+G.add_edge(ELEMENT.PARLIMENT, ELEMENT.FORENSIC_COMMITTEE, relation=RELATION.INSTITUTES)
+# Art. 20 p. 1
+G.add_edge(ELEMENT.PARLIMENT, ELEMENT.COMMITTEE, relation=RELATION.CHOOSES)
+# Art. 20 p. 2
+G.add_edge(ELEMENT.CHIEF_OF_COMMITTEE, ELEMENT.COMMITTEE, relation=RELATION.IS_MEMBER)
+G.add_edge(ELEMENT.VICE_CHIEF_OF_COMMITTEE, ELEMENT.COMMITTEE, relation=RELATION.IS_MEMBER)
+# Art. 20 p. 3
+G.add_edge(ELEMENT.COMMITTEE, ELEMENT.PRESIDIUM, relation=RELATION.CHOOSES)
+G.add_edge(ELEMENT.COMMITTEE, ELEMENT.PRESIDIUM, relation=RELATION.CANCELS)
+# Art. 21 p. 1
+G.add_edge(ELEMENT.PRESIDIUM, ELEMENT.REPRESENTATIVE, relation=RELATION.WARN, may = True)
+# Art. 23 p. 1
+G.add_edge(ELEMENT.PRESIDIUM, ELEMENT.REPRESENTATIVE, relation=RELATION.DECREASE_DIET, may = True)
+# Art. 24 p. 1
+G.add_edge(ELEMENT.MARSHALL, ELEMENT.REPRESENTATIVE, relation=RELATION.DECREASE_DIET, may = True)
+# Art. 25 p. 1
+G.add_edge(ELEMENT.MARSHALL, ELEMENT.REPRESENTATIVE, relation=RELATION.EXCLUDE_FROM_PRECEEDINGS, may = True)
+
 
 # Drawing the graph
 edge_labels = nx.get_edge_attributes(G,'relation')
 pos = nx.planar_layout(G)
 nx.draw_networkx_edge_labels(G, pos, edge_labels)
-nx.draw(G, with_labels = True)
+nx.draw(G, pos, with_labels = True)
 plt.show()
